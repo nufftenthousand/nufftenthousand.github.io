66cb4ddb1dd0cceebeca166e523b706a17e22399
System Administrator <root@natemba.(none)>
2011-11-08 22:23:48 -0500

    revamp

---
+++ js/application.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+		// $('.shiny').children().delay(3000).each(function(i){
+		// 				$(this).delay(i*150).fadeTo(400, 0.8);
+		// });
+		$('.shiny').hover(
+			function(){
+				$(this).children().each(function(i){
+						$(this).delay(i*55).fadeTo(200, 0.8);
+				})
+			},
+			function(){
+				$(this).children().each(function(i){
+						$(this).delay(i*55).fadeTo(200, 1);
+				});
+			}
+		);
+});
