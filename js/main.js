9b0bb4b334b2f8842186be4a5312df5236596c41
Nate Duffy <nate.duffy@gmail.com>
2016-01-19 12:37:14 -0500

    github and linkedin icons

--- js/main.js
+++ js/main.js
@@ -51,7 +51,53 @@ function emailIsValid() {
   return false;
 }
 
+function writeObfuscatedEmail() {
+  // Email obfuscator script 2.1 by Tim Williams, University of Arizona
+  // Random encryption key feature by Andrew Moulden, Site Engineering Ltd
+  // This code is freeware provided these four comment lines remain intact
+  // A wizard to generate this code is at http://www.jottings.com/obfuscator/
+  coded = "6BxG.9g001@QAB8s.uOA";
+  key = "bhlpuV0CEY3Gxn2UvwimP5sygXqMJaLSd6THor8AkIR1QDc4fBFW9etjOKzZ7N";
+  shift = coded.length;
+  link = "";
+  for ( i = 0; i < coded.length; i++) {
+    if ( key.indexOf(coded.charAt(i) )==-1) {
+      ltr = coded.charAt(i);
+      link += (ltr);
+    } else {
+      ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length;
+      link += (key.charAt(ltr));
+    }
+  }
+  $('.email').append('<a href="mailto:'+link+'" target="_blank">'+link+'</a>');
+  $('#formspree').attr('action','//formspree.io/'+link);
+}
+
+function writeObfuscatedPhone() {
+  // Email obfuscator script 2.1 by Tim Williams, University of Arizona
+  // Random encryption key feature by Andrew Moulden, Site Engineering Ltd
+  // This code is freeware provided these four comment lines remain intact
+  // A wizard to generate this code is at http://www.jottings.com/obfuscator/
+  coded = "8rV.y88.5nsr";
+  key = "vmF1khoZsif6ETpdOPWuMBH840zXUR9DtAYn5rSJeQGK3LIlyqxbc7ajVC2gNw";
+  shift = coded.length;
+  link = "";
+  for ( i = 0; i < coded.length; i++ ) {
+    if ( key.indexOf(coded.charAt(i))==-1 ) {
+      ltr = coded.charAt(i);
+      link += (ltr);
+    } else {
+      ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length;
+      link += (key.charAt(ltr));
+    }
+  }
+  $('.phone').append(link);
+}
+
 $(document).ready( function() {
+
+  writeObfuscatedEmail();
+  writeObfuscatedPhone();
 
   $('input:required').labelFor().append('<span class="form-note">(required)</span>');
 
